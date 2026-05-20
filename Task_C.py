ae4941272c0ae60ef1c02143f5de22e2ca184d9f
Niv Kushnir <nkushnir1802@cs.colman.ac.il>
2026-05-20 22:03:31 +0300

    added due dates to Task class

--- Task_C.py
+++ Task_C.py
@@ -1,12 +1,13 @@
 class Task:
-    def __init__(self,title,completed,priority):
+    def __init__(self,title,completed,priority,due_date):
         self.title = title
         self.completed = completed
         self.priority = priority
+        self.due_date = due_date
     
     def to_dict(self):
-        return {"title": self.title, "completed": self.completed,"priority": self.priority}
+        return {"title": self.title, "completed": self.completed,"priority": self.priority,"due_date": self.due_date}
     
     @classmethod
     def from_dict(cls,data):
-        return cls(data["title"],data["completed"],data["priority"])
+        return cls(data["title"],data["completed"],data["priority"],data["due_date"])
