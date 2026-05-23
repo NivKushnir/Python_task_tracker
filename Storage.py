8ae12b00aeee0ffbf2a5e1b9b8094ed46eab0e6b
Niv Kushnir <nkushnir1802@cs.colman.ac.il>
2026-05-23 11:08:18 +0300

    added notes in Storage.py

--- Storage.py
+++ Storage.py
@@ -1,13 +1,13 @@
 import json
 import Task
 from Task_C import Task
-
+#the function will take the tasks list and will convert it to a json file
 def save_tasks(tasks):
     data = [task.to_dict() for task in tasks]
     with open("tasks_list","w") as file:
         json.dump(data, file,indent=4)
     print("Tasks saved")
-
+#the function will convert the json file to adictinary and then to a list
 def load_tasks():
     try:
         with open("tasks_list","r") as file:
