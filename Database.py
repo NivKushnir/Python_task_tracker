a2fd76e7bf6ae19cab4f2a18934bacb1980d43ac
Niv Kushnir <nkushnir1802@cs.colman.ac.il>
2026-06-13 14:37:18 +0300

    Add SQLite database and task insertion support

--- Database.py
+++ Database.py
@@ -1,11 +1,37 @@
 import sqlite3
 
+#Creates a databse for the project
 def create_database():
     conn = sqlite3.connect("tasks.db")
 
-    crusor = conn.cursor()
+    cursor = conn.cursor()
 
-    crusor.execute(""" CREATE TASBLE IF NOT EXIST tasks(id INTEGER PRIMARY KEY AUTOINCREMENT,title TEXT NOT NULL,completed INTEGER NOT NULL,priority TEXT NOT NULL,due_date TEXT NOT NULL,category TEXT NOT NULL)""")
+    cursor.execute(""" CREATE TABLE IF NOT EXISTS tasks(id INTEGER PRIMARY KEY AUTOINCREMENT,title TEXT NOT NULL,completed INTEGER NOT NULL,priority TEXT NOT NULL,due_date TEXT NOT NULL,category TEXT NOT NULL)""")
 
     conn.commit()
+    conn.close()
+
+#Adds tasks in the DB
+def add_task(task):
+    conn = sqlite3.connect("tasks.db")
+    cursor = conn.cursor()
+
+    cursor.execute(""" INSERT INTO tasks 
+                   (title,completed,priority,due_date,category)
+                   VALUES(?,?,?,?,?)""",(task.title,int(task.completed),task.priority,task.due_date,task.category))
+    
+    conn.commit()
+    conn.close()
+
+def print_db():
+    conn = sqlite3.connect("tasks.db")
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM tasks")
+
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(row)
+
     conn.close()
--- main.py
+++ main.py
@@ -6,40 +6,40 @@ import Database as DB
 
 DB.create_database()
 root.mainloop()
+DB.print_db()
+# #The trackers menu
+# Menu = {1: "Add Task" ,2:"View Tasks" ,3:"Task completed", 4:"Show Statistics" ,5:"Filter Tasks" ,6:"Edit task" ,7:"Delete tasks" ,8:"Search tasks" ,0:"Exit"}
+# tasks = S.load_tasks() # loadind the json file to a list
+# print(Menu)
+# choice = input("What would you like to do: ") #getting an input of what the user would like to do
+# while(choice!="0"):
+#     if choice == "1":#adding a task
+#         T.add_task(tasks)
+#         print("Task added")
+#     elif choice == "2":#show all the tasks
+#         T.show_tasks(tasks)
+#     elif choice == "3":#the user completed a task and updating it 
+#         flag = T.task_complete(tasks)
+#         while(flag):
+#               print("Not a number, Please try again")
+#               T.task_complete(tasks)
+#         print("Task completed")
+#     elif choice == "4": #shows all the statistics of the user tasks 
+#         print("Your statistics: \n")
+#         T.show_statistics(tasks)
+#     elif choice == "5": # the user will filter what tasks he would like to see
+#         filtered_tasks = T.filter_tasks(tasks)
+#         T.show_tasks(filtered_tasks)
+#     elif choice == "6":
+#         T.edit_task(tasks)
+#     elif choice == "7":
+#         T.delete_tasks(tasks)
+#     elif choice == "8":
+#         T.show_tasks(T.search_tasks(tasks))
+#     else: print("Not a vaild option")
+#     print(Menu)
+#     choice = input("What would you like to do: ")
 
-#The trackers menu
-Menu = {1: "Add Task" ,2:"View Tasks" ,3:"Task completed", 4:"Show Statistics" ,5:"Filter Tasks" ,6:"Edit task" ,7:"Delete tasks" ,8:"Search tasks" ,0:"Exit"}
-tasks = S.load_tasks() # loadind the json file to a list
-print(Menu)
-choice = input("What would you like to do: ") #getting an input of what the user would like to do
-while(choice!="0"):
-    if choice == "1":#adding a task
-        T.add_task(tasks)
-        print("Task added")
-    elif choice == "2":#show all the tasks
-        T.show_tasks(tasks)
-    elif choice == "3":#the user completed a task and updating it 
-        flag = T.task_complete(tasks)
-        while(flag):
-              print("Not a number, Please try again")
-              T.task_complete(tasks)
-        print("Task completed")
-    elif choice == "4": #shows all the statistics of the user tasks 
-        print("Your statistics: \n")
-        T.show_statistics(tasks)
-    elif choice == "5": # the user will filter what tasks he would like to see
-        filtered_tasks = T.filter_tasks(tasks)
-        T.show_tasks(filtered_tasks)
-    elif choice == "6":
-        T.edit_task(tasks)
-    elif choice == "7":
-        T.delete_tasks(tasks)
-    elif choice == "8":
-        T.show_tasks(T.search_tasks(tasks))
-    else: print("Not a vaild option")
-    print(Menu)
-    choice = input("What would you like to do: ")
-
-#saves the tasks list to a json file
-S.save_tasks(tasks=tasks)
-print("Have a good day")
+# #saves the tasks list to a json file
+# S.save_tasks(tasks=tasks)
+# print("Have a good day")
